d4c1afe4555c8131db735dee8d4fbdc2001fc282
alunogabrielgava123 <pinheiros.casa@gmail.com>
2023-02-22 02:04:25 -0300

    Updates

---
+++ _next/static/1KV1uw9ulJknShOFgoNGA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/25a41dfc5c4fce6a.css","static/chunks/pages/index-2cd1b009de68f557.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
